af3b7a72fe65904e1789777027754dad3dc5123f
Oriol Angelet <uriaeri@gmail.com>
2025-03-08 16:29:39 +0100

    Initial commit

---
+++ SpaceBattleGameAC/SpaceBattleGameAC/SpaceBattleGameACApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceBattleGameACApp.swift
+//  SpaceBattleGameAC
+//
+//  Created by Uri on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SpaceBattleGameACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
